0726ca2cbd800e905db0788303a28a09b60b0030
chegrofelix <chegerofelix@gmail.com>
2021-04-29 09:36:37 +0300

    upadated homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './shared/material.module';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,38 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.scss']
-// })
-// export class HomeComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
 @Component({
   selector: 'app-home',
-  template: `
-    <section class="hero is-info is-fullheight is-bold">
-    <div class="hero-body">
-    <div class="container">
-
-      <h1 class="title">Home Page!</h1>
-
-    </div>
-    </div>
-    </section>
-  `,
-  styles: []
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-    constructor() { }
-  
-    ngOnInit(): void {
-    }
-  
+  constructor() { }
+
+  ngOnInit(): void {
   }
+
+}
+
+
