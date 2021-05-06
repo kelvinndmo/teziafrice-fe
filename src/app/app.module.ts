6b4d8faae17b9cbcbe697c5bba3cfb84ebd40008
ReginaWangui <reginahwangui801@gmail.com>
2021-05-07 00:47:06 +0300

    created home content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list'; 
+import {MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from'@angular/material/toolbar';
+import{MatCheckboxModule } from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule} from '@angular/material/form-field'
+import { MatInputModule} from '@angular/material/input'
+import { from } from 'rxjs';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { Routes } from '@angular/router';
+
+
+const appRoutes:Routes =[
+  {path:'', component: HomeComponent},
+  {path:'aboutus', component: AboutusComponent},
+    
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+   
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
